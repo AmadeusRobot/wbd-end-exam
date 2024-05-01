2b76dbb8d3a94968fed261b45ad7612df034677f
Anjali19991 <anjalimago999@gmail.com>
2024-05-01 09:02:15 +0530

    updates

--- src/server/config.js
+++ src/server/config.js
@@ -1,7 +1,20 @@
 import { config } from "dotenv";
+import mongoose from "mongoose";
 
-config()
+config();
 
-export const DB_URL = process.env.DB_URL || "=>"
+export const DB = process.env.DB_URL || "=>";
 
-export const PORT = process.env.PORT || 8080
+export const PORT = process.env.PORT || 8080;
+
+mongoose
+    .connect(DB, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => {
+        console.log("Connected to MongoDB");
+    })
+    .catch((error) => {
+        console.error("Error connecting to MongoDB:", error);
+    });
