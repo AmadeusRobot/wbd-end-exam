95e3456a1c0ee5faf662d65951183a441e398991
notRyuk <amadeusrobot.notryuk@gmail.com>
2024-05-01 10:40:22 +0530

    updates

--- src/client/hooks/fetcher.js
+++ src/client/hooks/fetcher.js
@@ -1,19 +1,9 @@
 import axios from "axios";
-import { useSelector } from "react-redux";
 import useSWR from "swr";
 import useSWRMutation from "swr/mutation";
-import { selectSession } from "../components/auth/authSlice";
 
 function useCreateRequest() {
-    const session = useSelector(selectSession);
-    if (!session) {
-        return axios.create();
-    }
-    return axios.create({
-        headers: {
-            authorization: `Bearer ${session.token}`,
-        },
-    });
+    return axios.create();
 }
 
 export function useGetter(url, config) {
